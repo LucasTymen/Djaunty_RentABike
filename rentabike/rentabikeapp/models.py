b8ea9d3cd016c871a8b106db662c6d450325e270
Lucas Tymen <lucas.tymen@gmail.com>
2023-07-20 17:34:10 +0200

    models - bikes

--- rentabike/rentabikeapp/models.py
+++ rentabike/rentabikeapp/models.py
@@ -6,3 +6,19 @@ TANDEM_SURCHARGE = 15.00
 ELECTRIC_SURCHARGE = 25.00
 
 # Create your models here.
+
+class Bike(models.Model):
+  STANDARD = "ST"
+  TANDEM = "TA"
+  ELECTRIC = "EL"
+  BIKE_TYPE_CHOICES = [
+    (STANDARD, "Standard"),
+    (TANDEM, "Tandem"),
+    (ELECTRIC, "Electric")
+  ]
+
+  bike_type = models.Charflied(max_length=2,choices=BIKE_TYPE_CHOICES,default=STANDARD)
+  color = models.Charfield(max_length=10,default="")
+
+  def __str__(self):
+    return self.bike_type + " - " + self.color
